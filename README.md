f7dfc13097f3435458d49a3131b43aa0d59e7a59
Paweł Szulczewski <pawel@szulczewski.org>
2015-09-20 21:28:13 +0200

    added option -t (outputs as a table) and -l (outputs as lines)

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # live_chess_ratings_cli
-Takes live chess ratings from http://www.2700chess.com/ and prints in a pretty ASCII table:
+Takes live chess ratings from http://www.2700chess.com/ and prints in
+a pretty ASCII table (option -t):
 
 ![example](https://raw.github.com/pawelszulczewski/live_chess_ratings_cli/master/example.png)
+
+or prints as tab separated lines (option -l)
+
--- live_chess_ratings_cli.py
+++ live_chess_ratings_cli.py
@@ -5,46 +5,71 @@
 # and prints in command-line
 #
 # GPLv2
-# psz, pawel <at> szulczewski <dot> org, march 2015
+# psz, pawel <at> szulczewski <dot> org, march-september 2015
 
 import urllib2
-import texttable
+import getopt
+import sys
 from bs4 import BeautifulSoup
-from sys import exit
 
-table_header = ["No", "↑↓", "Surname", "Country", "ELO", "ELO +-"]
 data = []
 
-try :
-    final_table = texttable.Texttable()
-    final_table.add_row(table_header)
-    
-    web_page = urllib2.urlopen("http://2700chess.com").read()
-    soup = BeautifulSoup(web_page, "lxml")
-    table = soup.find('table', {'id': 'live-ratings-table'})
-    rows = table.find_all('tr')
-    
-    for row in rows:
-       cols = row.find_all('td')
-       cols = [ele.text.strip() for ele in cols]
-       data.append([ele for ele in cols])
+def main (argv):
+    try :    
+        opts, args = getopt.getopt(argv,"tlg:")    
+        for opt, arg in opts:
+            if opt == '-t':
+                table_output = True
+                line_output  = None
+            elif opt == '-l':
+                table_output = None
+                line_output = True
+                
+        web_page = urllib2.urlopen("http://2700chess.com").read()
+        soup = BeautifulSoup(web_page, "lxml")            
 
-    for players in data:
-        d = []
-        for pl_data in players[0:6]:
-            if pl_data:
-                d.append(pl_data.splitlines()[0].encode('UTF8'))
-            else:
-                d.append("-")
-        if (len(d) == 6):
-            final_table.add_row(d)
+        if table_output:
+            import texttable
+            table_header = ["No", "↑↓", "Surname", "Country", "ELO", "ELO +-"]            
+            final_table = texttable.Texttable()
+            final_table.add_row(table_header)
+            
+        table = soup.find('table', {'id': 'live-ratings-table'})        
+        rows = table.find_all('tr')
+    
+        for row in rows:
+            cols = row.find_all('td')
+            cols = [ele.text.strip() for ele in cols]
+            data.append([ele for ele in cols])
 
-    print final_table.draw()
-except urllib2.HTTPError :
+        for players in data:
+            d = []
+            for pl_data in players[0:6]:
+                if pl_data:
+                    d.append(pl_data.splitlines()[0].encode('UTF8'))
+                else:
+                    d.append("-")
+            if (len(d) == 6):
+                if table_output:
+                    final_table.add_row(d)
+                else:
+                    print ('\t'.join (map (str, d)))
+
+        if table_output:
+            print final_table.draw()
+            
+    except getopt.GetoptError:
+          print 'live_chess_ratings_cli.py'
+          print '\t\t -t\t output as a table'
+          print '\t\t -l\t output as lines'
+          exit(3)
+    except urllib2.HTTPError :
         print("HTTPERROR!")
         exit(1)
-except urllib2.URLError :
+    except urllib2.URLError :
         print("URLERROR!")
         exit(2)
-        
-exit(0)
+
+if __name__ == "__main__":
+       main(sys.argv[1:])
+       exit(0)
